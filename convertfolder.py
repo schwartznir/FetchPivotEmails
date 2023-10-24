ae917d471681cef697ec439ec79b3b90b371e3c7
schwartznir <nir.schwartz@math.u-psud.fr>
2023-10-24 17:53:33 +0200

    Initial commit

---
+++ convertfolder.py
@@ -0,0 +1,41 @@
+import pdfplumber
+import pandas as pd
+
+# PDF extraction and data transformation
+pdf_path = "signed (1).pdf"
+
+
+def extract_data_from_pdf(pdf_path, page_num, maxscore=5):
+    data = []
+    with pdfplumber.open(pdf_path) as pdf:
+        page = pdf.pages[page_num - 1]  # Adjust page index since it's 0-based
+        table = page.extract_table()
+        if table:
+            for row_index, row in enumerate(table):
+                for col_index, cell in enumerate(row):
+                    if cell and 'X' in cell:
+                        data.append((row_index, abs(maxscore - col_index)))
+
+    return pd.DataFrame(data, columns=['Question number', 'Column Marked with X'])
+
+
+import os
+
+
+def generate_table(folder_path, maxscore=5, pagenum=1, qnum=22):
+    result = pd.DataFrame(columns=[f'Response to Question number {i}' for i in range(-1, qnum + 1)])
+    result = result.rename(columns={'Response to Question number -1': 'id'})
+    result = result.rename(columns={'Response to Question number 0': 'date'})
+    idx = 0
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            pdf_path = os.path.join(folder_path, filename)
+            df_page = extract_data_from_pdf(pdf_path, page_num=pagenum, maxscore=maxscore)
+            result.at[idx, 'id'], result.at[idx, 'date'] = filename.split('.')[0].split('|')
+            for jdx, row in df_page.iterrows():
+                col_name = f'Response to Question number {row["Question number"]}'
+                result.at[idx, col_name] = row['Column Marked with X']
+
+            idx += 1
+    # result.to_excel(f'Responses from page {pagenum}.xlsx')
+    return result
---
+++ readgmail.py
@@ -0,0 +1,108 @@
+import os
+import base64
+from typing import List
+import datetime
+import io
+from PyPDF2 import PdfReader
+from google_apis import create_service
+import re
+
+
+class GmailException(Exception):
+	"""gmail base exception class"""
+
+
+class NoEmailFound(GmailException):
+	"""no email found"""
+
+
+def search_emails(query_stirng: str, label_ids: List = None):
+	try:
+		message_list_response = service.users().messages().list(
+			userId='me',
+			labelIds=label_ids,
+			q=query_string
+		).execute()
+
+		message_items = message_list_response.get('messages')
+		next_page_token = message_list_response.get('nextPageToken')
+		
+		while next_page_token:
+			message_list_response = service.users().messages().list(
+				userId='me',
+				labelIds=label_ids,
+				q=query_string,
+				pageToken=next_page_token
+			).execute()
+
+			message_items.extend(message_list_response.get('messages'))
+			next_page_token = message_list_response.get('nextPageToken')
+		return message_items
+	except Exception as e:
+		raise NoEmailFound('No emails returned')
+
+
+def get_message_detail(message_id, msg_format='metadata', metadata_headers: List = None):
+	message_detail = service.users().messages().get(
+		userId='me',
+		id=message_id,
+		format=msg_format,
+		metadataHeaders=metadata_headers
+	).execute()
+	return message_detail
+
+
+if __name__ == '__main__':
+	CLIENT_FILE = 'client_secret.json'
+	API_NAME = 'gmail'
+	API_VERSION = 'v1'
+	SCOPES = ['https://mail.google.com/']
+	service = create_service(CLIENT_FILE, API_NAME, API_VERSION, SCOPES)
+	query_string = 'is:unread AND has:attachment'
+
+	save_location = os.getcwd()
+	email_messages = search_emails(query_string)
+	if email_messages is None:
+		print('No new data. End')
+		exit(0)
+	for msg in email_messages:
+		msg_id = msg['id']
+
+		# Get message date
+		msg_date = service.users().messages().get(userId='me', id=msg_id).execute()['internalDate']
+		date = datetime.datetime.fromtimestamp(int(msg_date) / 1000)
+		date_str = date.strftime("%d-%m-%Y")
+
+		message = service.users().messages().get(userId='me', id=msg_id).execute()
+
+		for part in message['payload']['parts']:
+			if part['filename'] and part['body']['attachmentId']:
+				attach_id = part['body']['attachmentId']
+				name = part['filename']
+				request = service.users().messages().attachments().get(
+					userId='me', messageId=msg_id, id=attach_id)
+				file_data = request.execute()
+				pdf_reader = PdfReader(io.BytesIO(base64.urlsafe_b64decode(file_data['data'])))
+				page_one = pdf_reader.pages[0]
+				text = page_one.extract_text()
+				match = re.search(r'(\d{4,})', text)
+				if match:
+					file_id = match.group(1)
+				else:
+					file_id = 'NaN'
+
+				# Construct filename
+				filename = f'{file_id}|{date_str}.pdf'
+
+				# Download attachment
+				request = service.users().messages().attachments().get(
+					userId='me', messageId=msg_id, id=attach_id).execute()
+				file_data = base64.urlsafe_b64decode(request.get('data').encode('UTF-8'))
+				with open(f'pdf_files/{filename}', 'wb') as thefile:
+					thefile.write(file_data)
+
+		service.users().messages().modify(userId='me', id=msg_id, body={
+			'removeLabelIds': ['UNREAD']
+		}).execute()
+
+	print("Attachments downloaded Successfully!")
